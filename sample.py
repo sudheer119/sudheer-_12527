4bac017ab858d26384df112eddf366521f456ff6
sudheer <nsudheer400@gmail.com>
2021-02-06 09:19:12 +0530

    sudheer7

--- sample.py
+++ sample.py
@@ -5,6 +5,10 @@ print("sudheer")
 print("KLH HYDERABAD")
 print("kl university")
 print("Sai amruth")
+
+
+
+
 for i in range(0,10):
   print(i)
 print("Hello word")
@@ -12,11 +16,4 @@ a=10
 b=10
 print(a+b)
 print("3")
-print("we aree heroes")
-list=[1,2,3,4]
-for i in list:
-  print(i)
-print("Sai Amruth")
-print("rnvhfnbijb")
-print("earngjtnbklt")
-print("git to pycharm")
+print("4")
